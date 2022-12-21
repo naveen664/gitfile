be9b0349541bb5a5164b1591d5525d62400cea81
naveen664 <naveenredn@gmail.com>
2022-12-21 11:14:13 +0530

    first message

--- Jyc.java
+++ Jyc.java
@@ -1,6 +1,7 @@
 class Jyc {
     public static void main(String[] args) {
     System.out.println("Hello all!");  
-    System.out.println("Hello friend!");     
+    System.out.println("Hello friend!");
+    System.out.println("First commit!");    
     }
 }
