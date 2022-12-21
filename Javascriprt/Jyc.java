1a855a92eac3cf7d74df318c694f79218517bdbc
naveen664 <naveenredn@gmail.com>
2022-12-21 11:06:25 +0530

    second commit success

--- Javascriprt/Jyc.java
+++
@@ -1,6 +0,0 @@
-class Jyc {
-    public static void main(String[] args) {
-    System.out.println("Hello all!");  
-    System.out.println("Hello friend!");     
-    }
-}
--- Jyc.java
+++ Jyc.java
@@ -1,5 +1,6 @@
 class Jyc {
     public static void main(String[] args) {
-        System.out.println("Hello all!"); 
+    System.out.println("Hello all!");  
+    System.out.println("Hello friend!");     
     }
 }
